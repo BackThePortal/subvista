d66722073a4c3e6cee6bfe7860e4241f38448ef1
BackThePortal <backtheportal@gmail.com>
2023-06-10 17:13:06 +0200

    10/06/2023 17-12

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,26 @@ const subInputValue = ref('');
 
 const showAppearance = ref(false);
 
-const appearance = reactive(localStorage.getItem('appearance') ? JSON.parse(localStorage.getItem('appearance')) : {
-	bg: '#000000', opacity: '170', blur: false, subs: '#DDDDDD', size: '18', pos: '5'
-})
+const videoSource = ref('yt');
+
+/**
+ *
+ * @type {Ref<UnwrapRef<HTMLScriptElement | null>>}
+ */
+const ytIframe = ref(null);
+
+const appearance = reactive(
+	localStorage.getItem('appearance')
+		? JSON.parse(localStorage.getItem('appearance'))
+		: {
+				bg: '#000000',
+				opacity: '170',
+				blur: false,
+				subs: '#DDDDDD',
+				size: '18',
+				pos: '5',
+		  }
+);
 
 const liveUpdate = ref(false);
 
@@ -25,16 +42,15 @@ const lastChange = useLastChanged(subInputValue, {
 
 const lastChangeAgo = useTimeAgo(lastChange);
 
-
 watch(appearance, () => {
-	if (windowVideo.value) updateTheme()
-})
+	if (windowVideo.value) updateTheme();
+});
 
 function updateTheme() {
 	const el = windowVideo.value?.document.getElementById('subs');
 	let bgColor = appearance.bg.split('').slice(1);
-	bgColor.push(parseInt(appearance.opacity).toString(16))
-	el.style.backgroundColor = '#' + bgColor.join('')
+	bgColor.push(parseInt(appearance.opacity).toString(16));
+	el.style.backgroundColor = '#' + bgColor.join('');
 	el.style.color = appearance.subs;
 	el.style.fontSize = appearance.size + 'px';
 	el.style.transform = `translateY(-${appearance.pos}rem)`;
@@ -61,7 +77,7 @@ const videoControls = {
 		getVideoElement().pause();
 		getVideoElement().dataset.playing = 'false';
 	},
-	play:  () => {
+	play: () => {
 		getVideoElement().play();
 		getVideoElement().dataset.playing = 'true';
 	},
@@ -74,14 +90,13 @@ function createWindow(e) {
 		windowVideo.value = null;
 	} else {
 		windowVideo.value = window.open(
-				"/videoHost.html",
-				undefined,
-				'popup,width=800,height=600,left=800'
+			'/videoHost.html',
+			undefined,
+			'popup,width=800,height=600,left=800'
 		);
 		setTimeout(() => {
-			updateTheme()
-		}, 100)
-
+			updateTheme();
+		}, 100);
 	}
 }
 
@@ -90,12 +105,12 @@ function handleFocus(e) {
 }
 
 function handleNewLine(e) {
-	subInputValue.value += '\n'
+	subInputValue.value += '\n';
 }
 
-
 function sendSubs(e) {
-	windowVideo.value.document.getElementById('subs').innerText = subInputValue.value;
+	windowVideo.value.document.getElementById('subs').innerText =
+		subInputValue.value;
 }
 
 function focusSubInput() {
@@ -107,7 +122,7 @@ function focusSubInput() {
 }
 
 function handleTemp(e) {
-	console.log(e.target.checked)
+	console.log(e.target.checked);
 }
 </script>
 
@@ -119,26 +134,73 @@ function handleTemp(e) {
 		<p class="text-xl text-slate-300">{{ windowVideo }}</p>
 	</header>
 	<p
-			v-html="
+		v-html="
 			windowVideo
 				? `La finestra s'ha creat. Pots controlar el vídeo des d'aquí mateix.`
 				: 'Subvista funciona creant una nova finestra on es reprodueix el vídeo. ' +
 				  'Els subtítols que escriguis hi aniran apareixent. <br>Comença pujant el vídeo que vulguis subtitular.'
 		"
 	></p>
-	<input
-			v-if="!windowVideo"
+	<template v-if="!windowVideo">
+		<h3>
+			<input
+				type="radio"
+				id="fileRadio"
+				name="source"
+				value="file"
+				v-model="videoSource"
+			/><label
+				class="transition"
+				:class="[videoSource === 'file' ? '' : 'opacity-60 hover:opacity-80']"
+				for="fileRadio"
+				>Des d'un fitxer</label
+			>
+		</h3>
+		<input
 			ref="videoInput"
-			class="my-2 cursor-pointer rounded-lg bg-slate-700 p-3 text-slate-300 transition file:cursor-pointer file:rounded-md file:border file:border-0 file:!border-b-2 file:!border-r-2 file:!border-r-2 file:!border-r-2 file:border-b-slate-200/100 file:border-r-slate-200/100 file:border-r-slate-200/100 file:border-r-slate-200/100 file:bg-slate-800 file:font-bold file:text-slate-200 file:transition hover:bg-slate-700 hover:bg-slate-800 active:bg-slate-600 active:ring active:ring-slate-400"
+			class="my-2 rounded-lg bg-slate-700 p-3 text-slate-300 transition file:cursor-pointer file:rounded-md file:border file:border-0 file:!border-b-2 file:!border-r-2 file:!border-r-2 file:!border-r-2 file:border-b-slate-200/100 file:border-r-slate-200/100 file:border-r-slate-200/100 file:border-r-slate-200/100 file:bg-slate-800 file:font-bold file:text-slate-200 file:transition disabled:opacity-40"
+			:class="{
+				'cursor-pointer file:cursor-default hover:bg-slate-700 hover:bg-slate-800 active:bg-slate-600 active:ring active:ring-slate-400':
+					videoSource === 'file',
+			}"
+			:disabled="videoSource !== 'file'"
 			type="file"
 			name="Vídeo"
 			id="videoInput"
-	/>
+		/>
+		<h3>
+			<input
+				type="radio"
+				id="ytRadio"
+				name="source"
+				value="yt"
+				v-model="videoSource"
+			/><label
+				class="transition"
+				:class="[videoSource === 'yt' ? '' : 'opacity-60 hover:opacity-80']"
+				for="ytRadio"
+				>Des de YouTube</label
+			>
+		</h3>
+		<input
+			class="w-[18.5rem] placeholder-slate-300/50 transition disabled:opacity-40"
+			type="text"
+			id="ytsrc"
+			placeholder="youtube.com/watch?v=dQw4w9WgXcQ"
+			:disabled="videoSource !== 'yt'"
+		/>
+	</template>
+
 	<h3 v-else>Finestra</h3>
 	<div class="flex gap-2">
 		<button
-				:class="windowVideo ? 'bg-red-700/60 active:bg-red-700 active:ring-red-500' : 'bg-green-700/60 active:bg-green-700 active:ring-green-500'"
-				@click="createWindow">
+			:class="
+				windowVideo
+					? 'bg-red-700/60 active:bg-red-700 active:ring-red-500'
+					: 'bg-green-700/60 active:bg-green-700 active:ring-green-500'
+			"
+			@click="createWindow"
+		>
 			{{ windowVideo ? 'Tancar' : 'Crear finestra' }}
 		</button>
 		<button v-if="windowVideo" @click="handleFocus">Portar-la al davant</button>
@@ -152,38 +214,64 @@ function handleTemp(e) {
 			<button @click="handleRestart">Des del principi</button>
 		</div>
 		<h3>Subtítols</h3>
-		<button class=" font-semibold" @click="showAppearance = !showAppearance">Aparença</button><br />
+		<button class="font-semibold" @click="showAppearance = !showAppearance">
+			Aparença</button
+		><br />
 		<template v-if="showAppearance">
 			<label for="colorSubs">Color del text</label>
-			<input type="color" id="colorSubs" v-model="appearance.subs"/>
-			<br/>
+			<input type="color" id="colorSubs" v-model="appearance.subs" />
+			<br />
 			<label for="colorBg">Color del fons</label>
-			<input type="color" id="colorBg" v-model="appearance.bg"/>
-			<br/>
+			<input type="color" id="colorBg" v-model="appearance.bg" />
+			<br />
 			<label for="bgOpacity">Opacitat del fons</label>
-			<input id="bgOpacity" type="range" min="0" max="255" v-model="appearance.opacity">
-			<br/>
+			<input
+				id="bgOpacity"
+				type="range"
+				min="0"
+				max="255"
+				v-model="appearance.opacity"
+			/>
+			<br />
 			<label for="bgBlur">Fons difuminat</label>
-			<input id="bgBlur" type="checkbox" @change="(e) => appearance.blur = e.target.checked">
-			<br/>
+			<input
+				id="bgBlur"
+				type="checkbox"
+				:checked="appearance.blur"
+				@change="(e) => (appearance.blur = e.target.checked)"
+			/>
+			<br />
 			<label for="subSize">Mida del text</label>
-			<input id="subSize" type="range" min="8" max="24" v-model="appearance.size"/>
-			<br/>
+			<input
+				id="subSize"
+				type="range"
+				min="8"
+				max="24"
+				v-model="appearance.size"
+			/>
+			<br />
 			<label for="subPos">Posició del text</label>
-			<input id="subPos" type="range" min="0" max="30" step="0.25" v-model="appearance.pos"/>
+			<input
+				id="subPos"
+				type="range"
+				min="0"
+				max="30"
+				step="0.25"
+				v-model="appearance.pos"
+			/>
 		</template>
 
 		<div class="flex w-full flex-col place-content-center items-center gap-4">
 			<textarea
-					class="subinput mt-2"
-					ref="subInputArea"
-					v-model="subInputValue"
-					@keydown.enter.exact.prevent="sendSubs"
-					@keydown.enter.alt.prevent="handleNewLine"
+				class="subinput mt-2"
+				ref="subInputArea"
+				v-model="subInputValue"
+				@keydown.enter.exact.prevent="sendSubs"
+				@keydown.enter.alt.prevent="handleNewLine"
 			/>
 		</div>
 
-		<br/>
+		<br />
 		<p v-if="!liveUpdate && !allowEnter">
 			Utilitza la tecla <kbd>Enter</kbd> per mostrar els subtítols. Utilitza
 			<kbd>Alt + Enter</kbd> per introduir un salt de línia.
@@ -194,7 +282,6 @@ function handleTemp(e) {
 
 <style scoped>
 .subinput {
-	@apply h-24 w-96 rounded-md bg-slate-600 p-2 text-lg text-white outline-0 ring-slate-300 focus:ring-2 shadow-2xl;
+	@apply h-24 w-96 rounded-md bg-slate-600 p-2 text-lg text-white shadow-2xl outline-0 ring-slate-300 focus:ring-2;
 }
-
 </style>
